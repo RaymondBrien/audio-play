5948e65f12aacf6f00e3fe252806beb03704133b
RaymondBrien <raytbrien@gmail.com>
2025-01-04 18:40:23 +0000

    Add initial functionality

---
+++ default_params.py
@@ -0,0 +1,11 @@
+
+class DefaultParams:
+    # Default parameters for the simulation
+    # Number of agents
+    NUM_AGENTS = 100
+    # Number of steps
+    NUM_STEPS = 1000
+    # Number of steps per test
+    NUM_STEPS_PER_TEST = 100
+    # Number of tests
+    NUM_TESTS = 10

683a869c0b3c6ea0b4bd7cbe79bfb6a9eba6b833
RaymondBrien <raytbrien@gmail.com>
2025-02-08 11:28:17 +0000

    Add base default class for visuals

---
+++ src/visual/utils/DefaultSetupVisual.py
@@ -0,0 +1,28 @@
+import attr
+import logging
+
+@attr.s(auto_attribs=True)
+class DefaultSetupVisual:
+   """
+   canvas
+   width: int
+   height: int
+
+   min_freq
+   max_freq
+
+   stack - keep all defaults here instead?
+
+   annimation_active : bool
+   annimation refresh rate (spped?)
+   placement
+   size
+   annimation_duration
+   annimation_type
+
+   base Color class - needed?
+   hex (default)
+   transparency
+   stack position
+   animate
+   """
